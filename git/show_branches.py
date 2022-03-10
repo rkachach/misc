10409c09f48785c47fe3c96f2ebb358861465370
Redouane Kachach <rkachach@redhat.com>
2022-03-10 12:24:11 +0100

    Adding new column to show merged branches

--- git/show_branches.py
+++ git/show_branches.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+import argparse
 from sys import argv
 from prettytable import PrettyTable
 from subprocess import check_output
@@ -7,25 +8,39 @@ import os
 import sys
 
 def format_table(table):
-    table.field_names = ["branch", "description"]
+    table.field_names = ["branch", "description", "merged"]
     table.align["branch"] = "l"
     table.align["description"] = "l"
+    table.align["merged"] = "l"
 
-def show_branches(prefix):
+def ls_all_branches():
+    try:
+        return check_output(['git', 'branch']).decode('utf')
+    except:
+        sys.exit(1)
+
+def ls_merged_branches(branch_name):
+    try:
+        return check_output(['git', 'branch', '--merged', branch_name]).decode('utf')
+    except:
+        sys.exit(1)
+
+def show_branches(prefix, main_branch):
     table = PrettyTable()
     format_table(table)
     G = "\033[0;32;40m" # GREEN
     N = "\033[0m" # Reset
+    Y = "\033[0;33;40m" # Yellow
+    B = "\033[0;34;40m" # Blue
 
-    try:
-        branches = check_output(['git', 'branch']).decode('utf')
-    except:
-        sys.exit(1)
-
+    branches = ls_all_branches()
+    merged_branches = ls_merged_branches(main_branch)
+    
     for branch in branches.splitlines():
         if prefix not in branch:
             continue
 
+        merged = branch in merged_branches
         curr_branch = '*' in branch
         branch_name = branch.strip() if not curr_branch else branch.strip().split(' ')[1]
         try:
@@ -33,14 +48,22 @@ def show_branches(prefix):
             desc = os.linesep.join([s for s in output.splitlines() if s])
         except:
             desc = '---'
+
+        bmerged = "YES" if merged else "NO"
         if curr_branch:
-            table.add_row([G+branch_name, desc+N])
+            table.add_row([G+branch_name, desc, bmerged+N])
+        elif merged:
+            table.add_row([Y+branch_name, desc, bmerged+N])
         else:
-            table.add_row([N+branch_name+N, desc+N])
+            table.add_row([N+branch_name+N, desc, bmerged+N])
+
 
     # Done
     print(table)
 
 if __name__ == '__main__':
-    prefix = argv[1] if len(argv) > 1 else ''
-    show_branches(prefix)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--prefix', default='')
+    parser.add_argument('-b', '--branch', help='main branch', default='master')
+    args = parser.parse_args()
+    show_branches(args.prefix, args.branch)
